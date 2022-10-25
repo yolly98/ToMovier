6876bfaee24598acebcf25811147ea7be29b62a2
gianluca gemini <gianlucagemini98@gmail.com>
2022-10-25 17:00:26 +0200

    updated style of the item section

--- src/components/js/ItemMenu.js
+++ src/components/js/ItemMenu.js
@@ -34,19 +34,19 @@ class Card extends Component{
             <div id="item-section">
                 <div style={{backgroundColor: '#6b8ea2', display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '1rem', borderRadius: '1rem'}}>
                     <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
-                        <label className="item-label">Aggiorna elemento</label>
+                        <label className="item-label" style={{fontWeight: 'bold'}}>Aggiorna elemento</label>
                         <img src={cross} style={{width: '1.5rem', cursor: 'pointer'}} onClick={()=>this.onCancel()}/>
                     </div>
-                    <div style={{width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'flex-start'}}>
+                    <div style={{width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'flex-start', marginTop: '1.5rem'}}>
                         <img src={coraline} style={{height: '15rem', marginRight: '2rem'}}/>
                         <div>
                             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '1.5rem'}}>
-                                <label className="item-label">Titolo</label>
-                                <input className='item-text' type="text" />
+                                <label className="item-label" style={{marginRight: '1rem'}}>Titolo</label>
+                                <input id="item-title" className='item-text' type="text" />
                             </div>
                             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '1.5rem'}}>
-                                <label className="item-label">Genere</label>
-                                <input className='item-text' type="text" />
+                                <label className="item-label" style={{marginRight: '0.4rem'}}>Genere</label>
+                                <input id="item-genre" className='item-text' type="text" />
                             </div>
                             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginBottom: '1.5rem'}}>
                                 <label className="item-label">Voto</label>
@@ -64,33 +64,33 @@ class Card extends Component{
                                 </div>
                             </div>
                             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '1.5rem'}}>
-                                <label className="item-label">Preferito</label>
-                                <img src={notfavorite} style={{width: '1.5rem'}}/>
+                                <label className="item-label"  style={{marginRight: '0.5rem'}}>Preferito</label>
+                                <img id="item-favorite" src={notfavorite} style={{width: '1.5rem'}}/>
                             </div>
                             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '1.5rem'}}>
-                                <label className="item-label">Visto</label>
-                                <img src={watched} style={{width: '1.5rem'}}/>
-                                <img src={watching} style={{width: '1.5rem'}}/>
-                                <img src={towatch} style={{width: '1.5rem'}}/>
+                                <label className="item-label"  style={{marginRight: '0.5rem'}}>Visto</label>
+                                <img id="item-watched" src={watched} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
+                                <img id="item-watching" src={watching} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
+                                <img id="item-towatch" src={towatch} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
                             </div>
                             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: '1.5rem'}}>
-                                <label className="item-label">Tipo</label>
-                                <img src={series} style={{width: '1.5rem'}}/>
-                                <img src={film} style={{width: '1.5rem'}}/>
+                                <label className="item-label" style={{marginRight: '0.5rem'}}>Tipo</label>
+                                <img id="item-series" src={series} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
+                                <img id="item-film" src={film} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
                             </div>
                         </div>
                     </div>
                     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '1.5rem'}}>
                         <label className="item-label">URL image</label>
-                        <input className='item-text' type="text"/>
+                        <input className='item-text' type="text" style={{marginBottom: '1.5rem'}}/>
                         <label className="item-label">Piattaforma</label>
-                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', backgroundColor: '#bdcbdd', borderRadius: '0.5rem', padding: '0.5rem'}}>
-                            <img src={netflix} style={{width: '1.5rem'}}/>
-                            <img src={netflix} style={{width: '1.5rem'}}/>
-                            <img src={netflix} style={{width: '1.5rem'}}/>
-                            <img src={netflix} style={{width: '1.5rem'}}/>
-                            <img src={netflix} style={{width: '1.5rem'}}/>
-                            <img src={netflix} style={{width: '1.5rem'}}/>
+                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-start', backgroundColor: '#bdcbdd', borderRadius: '0.5rem', padding: '0.5rem', width: '90%'}}>
+                            <img src={netflix} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
+                            <img src={netflix} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
+                            <img src={netflix} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
+                            <img src={netflix} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
+                            <img src={netflix} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
+                            <img src={netflix} style={{width: '1.5rem', marginRight: '0.5rem'}}/>
                         </div>
                     </div>
                     <button style={{borderRadius: '0.5rem'}} onClick={()=>this.onSave()}>SALVA</button>
